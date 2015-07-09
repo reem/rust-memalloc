91c61e59b8e4907656e37c37280683700741695e
Jonathan Reem <jonathan.reem@gmail.com>
2015-07-08 23:09:43 -0700

    (test) Add tests for memalloc's APIs.

--- src/lib.rs
+++ src/lib.rs
@@ -79,3 +79,80 @@ fn ptr_from_vec(mut buf: Vec<u8>) -> *mut u8 {
     ptr
 }
 
+#[cfg(test)]
+mod tests {
+    use std::ptr;
+    use {allocate, reallocate, deallocate, empty};
+
+    #[test]
+    fn test_empty() {
+        let ptr = empty();
+        assert!(ptr != ptr::null_mut());
+    }
+
+    #[test]
+    fn test_allocate() {
+        let buffer = unsafe { allocate(8) };
+
+        assert!(buffer != ptr::null_mut());
+
+        unsafe {
+            ptr::write(buffer.offset(0), 8);
+            ptr::write(buffer.offset(1), 4);
+            ptr::write(buffer.offset(3), 5);
+            ptr::write(buffer.offset(5), 3);
+            ptr::write(buffer.offset(7), 6);
+
+            assert_eq!(ptr::read(buffer.offset(0)), 8);
+            assert_eq!(ptr::read(buffer.offset(1)), 4);
+            assert_eq!(ptr::read(buffer.offset(3)), 5);
+            assert_eq!(ptr::read(buffer.offset(5)), 3);
+            assert_eq!(ptr::read(buffer.offset(7)), 6);
+        };
+
+        unsafe { deallocate(buffer, 8); }
+
+        // Try a large buffer
+        let buffer = unsafe { allocate(1024 * 1024) };
+        assert!(buffer != ptr::null_mut());
+
+        unsafe {
+            ptr::write(buffer.offset(1024 * 1024 - 1), 12);
+            assert_eq!(ptr::read(buffer.offset(1024 * 1024 - 1)), 12);
+        };
+
+        unsafe { deallocate(buffer, 1024 * 1024); }
+    }
+
+    #[test]
+    fn test_reallocate() {
+        let mut buffer = unsafe { allocate(8) };
+        assert!(buffer != ptr::null_mut());
+
+        buffer = unsafe { reallocate(buffer, 8, 16) };
+        assert!(buffer != ptr::null_mut());
+
+        unsafe {
+            // Put some data in the buffer
+            ptr::write(buffer.offset(0), 8);
+            ptr::write(buffer.offset(1), 4);
+            ptr::write(buffer.offset(5), 3);
+            ptr::write(buffer.offset(7), 6);
+        };
+
+        // Allocate so in-place fails.
+        unsafe { allocate(128) };
+
+        buffer = unsafe { reallocate(buffer, 16, 32) };
+        assert!(buffer != ptr::null_mut());
+
+        unsafe {
+            // Ensure the data is still there.
+            assert_eq!(ptr::read(buffer.offset(0)), 8);
+            assert_eq!(ptr::read(buffer.offset(1)), 4);
+            assert_eq!(ptr::read(buffer.offset(5)), 3);
+            assert_eq!(ptr::read(buffer.offset(7)), 6);
+        };
+    }
+}
+
